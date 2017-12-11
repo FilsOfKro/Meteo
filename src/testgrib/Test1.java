e48d50f648052feed30bd80b0cde5e8cde490079
Zglou <luc.rou@hotmail.fr>
2017-12-11 11:20:16 +0100

    gibribribjeg

--- src/testgrib/Test1.java
+++ src/testgrib/Test1.java
@@ -23,27 +23,26 @@ public class Test1 {
               GribRecordGDS r2 = grb.getGrids()[0];
               GribRecord ventU = grb.getRecord(1);
               GribRecord ventV = grb.getRecord(2);
-   
+              
               int nbx = r2.getGridNX();
               int nby = r2.getGridNY();
-              System.err.println(ventU.toString());
               for (int i = 0; i < nbx; i++)
               {
                   for (int j = 0; j < nby; j++)
                   {
-                      // affichage des la donnee (i,j)
+                      // affichage des donnees (i,j)
                       try
                       {
                         System.out.println("description ventu :"+ventU.getDescription());
 
-                        System.out.print("["+ventU.getGridCoords()[0]+":"+ventU.getGridCoords()[1]+"] : ");
+                        System.out.print("["+ventU.getGridCoords()[i]+":"+ventU.getGridCoords()[j]+"] : ");
                         System.out.print("time:"+ventU.getTime().getTime()+"::");
                           System.out.println(ventU.getValue(i, j) + " ");
                           
                           System.out.println();
 
                           System.out.println("description ventV :"+ventV.getDescription());
-                          System.out.print("["+ventV.getGridCoords()[0]+":"+ventV.getGridCoords()[1]+"] : ");
+                          System.out.print("["+ventV.getGridCoords()[i]+":"+ventV.getGridCoords()[j]+"] : ");
                           System.out.print(" time: "+ventU.getTime().getTime()+" :: ");
 
                           System.out.println(ventV.getValue(i, j) + " ");
@@ -62,6 +61,12 @@ public class Test1 {
               System.out.println(ventV.getUnit());
               // description de la donnee
               System.out.println(ventU.getPDS());
+              System.out.println(grb.toString());
+System.out.println( grb.getGrids().length);
+System.out.println( r2.getGridDX());
+System.out.println( r2.getGridDY());
+
+
           }
           catch (FileNotFoundException e)
           {
