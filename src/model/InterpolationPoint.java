66a3dc5105bff24e823e33e54d75675da9040af4
Sébastien Palud <palud.sebastien@gmail.com>
2018-03-21 14:09:06 +0100

    switch Grille parameter to Prevision parameter
    This way, calling the interpolate method wont call MeteoFacade and thus
    wont start a new frame in a test environment

--- src/model/InterpolationPoint.java
+++ src/model/InterpolationPoint.java
@@ -12,46 +12,46 @@ public class InterpolationPoint {
 	
 	/**
 	 * function that allows the user to interpolate a point 
-	 * @param g the grid of winds to see if the point is in it
+	 * @param p.getGrille() the grid of winds to see if the point is in it
 	 * @param x the longitude of the point to interpolate
 	 * @param y the latitude of the point to interpolate
 	 * @param d the date of the point to interpolate
 	 * @return true if the interpolation has been made, false else
 	 */
 	
-	public boolean InterpolateOneWind(Grille g, double x, double y, Date d) {
+	public boolean InterpolateOneWind(Prevision p, double x, double y, Date d) {
 		
 		boolean ret = true;
 		
-		if((x < g.getLonHautGauche()) && (y < g.getLatHautGauche())) {
+		if((x < p.getGrille().getLonHautGauche()) && (y < p.getGrille().getLatHautGauche())) {
 			ret = false;
 		}
-		if((x < (g.getLonHautGauche()+(g.getDx()*g.getNbx()))) && (y < (g.getLonHautGauche()+(g.getDy()*g.getNby())))) {
+		if((x < (p.getGrille().getLonHautGauche()+(p.getGrille().getDx()*p.getGrille().getNbx()))) && (y < (p.getGrille().getLonHautGauche()+(p.getGrille().getDy()*p.getGrille().getNby())))) {
 			ret = false;
 		}
 		
 		if(ret) {
 			
-			//on positionne le point à interpoler dans la grille
-			double posx = x-g.getLonHautGauche();
-			double posy = y-g.getLatHautGauche();
+			//on positionne le point ï¿½ interpoler dans la grille
+			double posx = x-p.getGrille().getLonHautGauche();
+			double posy = y-p.getGrille().getLatHautGauche();
 			
 			//on cherche entre quel colonne et ligne se trouve le point
-			double posxGrille = posx/g.getDx();
-			double posyGrille = posy/g.getDy();
+			double posxGrille = posx/p.getGrille().getDx();
+			double posyGrille = posy/p.getGrille().getDy();
 			
 			//recherche des points voisin
 			
 			int xB = (int) posxGrille;
-			int xBB = (int) (posxGrille+g.getDx());
+			int xBB = (int) (posxGrille+p.getGrille().getDx());
 			
 			int yA = (int) posyGrille;
-			int yAA = (int) (posyGrille + g.getDy());
+			int yAA = (int) (posyGrille + p.getGrille().getDy());
 			
-			//creation du vent creer par interpolation grace au coordonnées
-			PrevisionParDate prev = MeteoFacade.getInstance().getCurrentPrevision().getPrevisionParDate(d);
+			//creation du vent creer par interpolation grace au coordonnï¿½es
+			PrevisionParDate prev = p.getPrevisionParDate(d);
 			if(prev != null) {
-				//https://fr.wikihow.com/faire-une-double-interpolation-linéaire
+				//https://fr.wikihow.com/faire-une-double-interpolation-linï¿½aire
 				double dir = (((xBB-posxGrille)/(xBB-xB))*prev.getVents()[yA][xB].getDirection()+((posxGrille-xB)/(xBB-xB))*prev.getVents()[yA][xBB].getDirection())*((yAA-posyGrille)/(yAA-yA))+ (((xBB-posxGrille)/(xBB-xB))*prev.getVents()[yAA][xB].getDirection()+((posxGrille-xB)/(xBB-xB))*prev.getVents()[yAA][xBB].getDirection())*((posyGrille-yA)/(yAA-yA));
 				double vitesse = (((xBB-posxGrille)/(xBB-xB))*prev.getVents()[yA][xB].getVitesse()+((posxGrille-xB)/(xBB-xB))*prev.getVents()[yA][xBB].getVitesse())*((yAA-posyGrille)/(yAA-yA))+ (((xBB-posxGrille)/(xBB-xB))*prev.getVents()[yAA][xB].getVitesse()+((posxGrille-xB)/(xBB-xB))*prev.getVents()[yAA][xBB].getVitesse())*((posyGrille-yA)/(yAA-yA));
 				wind = new Vent(vitesse, dir, 4);
