2c6998b309770c596ec3fa346bba4bfa5e4d8698
Sébastien Palud <palud.sebastien@gmail.com>
2018-03-23 15:45:01 +0100

    Interfaces Vecteur et VecteurCalcul

---
+++ src/math/Vecteur.java
@@ -0,0 +1,12 @@
+package math;
+
+public interface Vecteur {
+
+	public double getComposanteU();
+	
+	public double getComposanteV();
+	
+	public double getVitesse();
+	
+	public double getDirection();
+}
--- src/math/VecteurCalcul.java
+++ src/math/VecteurCalcul.java
@@ -2,4 +2,25 @@ package math;
 
 public interface VecteurCalcul {
 
+	  /**
+	   * Calcule la direction en fonction des deux vecteurs en entrée
+	   * La direction est exprimée en degrés
+	   * @param vecteurU
+	   * @param vecteurV
+	   * @return
+	   */
+	  public double calculerDirection(double vecteurU, double vecteurV);
+	  
+	  /**
+	   * Calcule la vitesse en fonction des deux vecteurs en entrée
+	   * La vitesse est calculée en mètres par seconde
+	   * @param vecteurU
+	   * @param vecteurV
+	   * @return
+	   */
+	  public double calculerVitesse(double vecteurU, double vecteurV);
+	  
+	  public double calculerComposanteU(double vitesse, double direction);
+	  
+	  public double calculerComposanteV(double vitesse, double direction);
 }
